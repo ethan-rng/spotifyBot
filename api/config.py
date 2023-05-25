08c334870ac66b36ba608fd724be60e7092c176e
ethan-rng <116045008+daRongGuy@users.noreply.github.com>
2023-05-25 17:33:27 -0400

    reconfigured files

---
+++ api/config.py
@@ -0,0 +1,4 @@
+spotifyClientID = "2641e6df75754e07bd9bce9c09f0109a"
+spotifyClientSecret = "10385940dd4843db878e8de6af429ddc"
+youtubeAPI = "hi"
+
---
+++ src/components/Screens/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="flex flex-row">
+      <div className='flex flex-col justify-center items-center pt-10 pl-10'>
+          <div className='w-10 h-10 rounded-full bg-[#E1E9D8]' />
+          <div className='w-2.5 sm:h-80 h-40 green-gradient' />
+      </div>
+      
+      <div className='text-secondary'>
+        asdf  
+
+      </div>
+
+  </div>
+  )
+}
+
+export default Home
+
