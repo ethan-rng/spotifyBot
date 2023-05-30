adeea1b22f572204542597ef4df02832bcf3a1d1
ethan-rng <116045008+daRongGuy@users.noreply.github.com>
2023-05-29 23:41:56 -0400

    stashing minor changes

--- apiNew/app.py
+++ apiNew/app.py
@@ -1,2 +1,27 @@
-from flask import Flask, request, send_file, Response
+from apiKeys import *
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+from flask import Flask, request, send_file, Response, render_template
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+
+# from SpotifyScraper import SpotifyScraper
+
+# FLASK API Boilerplate
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# TESTING HOMEPAGE FOR CONNECTING
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+class Song(Resource):
+    pass
+
+class 
